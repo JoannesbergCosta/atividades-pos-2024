4d30dd279f198a1e154a2f630cf0cdfaa83de24f
joannesberg <joannesberg.100@gmail.com>
2024-08-07 16:50:32 -0300

    .

--- parses/cardapio.py
+++ parses/cardapio.py
@@ -1,18 +1,19 @@
 from xml.dom.minidom import parse
 
-# Carrega o arquivo XML
+
 dom = parse("parses/cardapio.xml")
 cardapio = dom.documentElement
 
-# Obtém a lista de pratos
+
+
 pratos = cardapio.getElementsByTagName('prato')
 
-# Exibe os pratos
+
 for i, prato in enumerate(pratos, 1):
     nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
     print(f'{i} - {nome}')
 
-# Solicita o ID do prato ao usuário
+
 try:
     id_p = int(input("Digite o ID do prato para saber mais: "))
     prato = pratos[id_p - 1]
