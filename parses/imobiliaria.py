96080c826df20ca2438999c8b9c89e9b5f7638d3
joannesberg <joannesberg.100@gmail.com>
2024-08-09 13:26:04 -0300

    atividade imobiliaria parses feita

--- parses/imobiliaria.py
+++ parses/imobiliaria.py
@@ -1,111 +1,47 @@
+from xml.dom.minidom import parse
 import json
+dom = parse('parses/imobiliaria.xml')
+imobiliaria = dom.documentElement
+imoveis = imobiliaria.getElementsByTagName('imovel')
+list_imobiliaria = []
+dict_imobiliaria = {}
 
-# Estrutura JSON convertida
-imobiliaria = {
-    "imobiliaria": {
-        "imovel": [
-            {
-                "descricao": "Apartamento 5 quartos, centro da cidade, com 1 banheiro",
-                "proprietario": {
-                    "nome": "Ryan Andrade",
-                    "email": "Ryan.and@email.com",
-                    "telefone": [
-                        "5849-5698",
-                        "9984-8956"
-                    ]
-                },
-                "endereco": {
-                    "rua": "Rua das Graças",
-                    "bairro": "Centro",
-                    "cidade": "Rio de Janeiro",
-                    "numero": "584"
-                },
-                "caracteristicas": {
-                    "tamanho": "80m²",
-                    "numQuartos": 7,
-                    "numBanheiros": 4
-                },
-                "valor": 300
-            },
-            {
-                "descricao": "Casa ampla com jardim",
-                "proprietario": {
-                    "nome": "Maria das Graças",
-                    "telefone": "8495-6321"
-                },
-                "endereco": {
-                    "rua": "Rua das Palmeiras",
-                    "bairro": "JardimMassai",
-                    "cidade": "Campina Grande"
-                },
-                "caracteristicas": {
-                    "tamanho": "150m²",
-                    "numQuartos": 3,
-                    "numBanheiros": 5
-                },
-                "valor": 4500000000000
-            },
-            {
-                "descricao": "casa em um condominio fechado",
-                "proprietario": {
-                    "nome": "Fernanda Lima",
-                    "email": "fernanda.lima@email.com",
-                    "telefone": "8795-8541"
-                },
-                "endereco": {
-                    "rua": "Avenida 4",
-                    "bairro": "Zona Sul",
-                    "cidade": "Rio de Janeiro",
-                    "numero": "4567"
-                },
-                "caracteristicas": {
-                    "tamanho": "150m²",
-                    "numQuartos": 4,
-                    "numBanheiros": 3
-                },
-                "valor": 689000
-            },
-            {
-                "descricao": "Studio moderno e aconchegante",
-                "proprietario": {
-                    "nome": "Joannes",
-                    "telefone": "4587-8971"
-                },
-                "endereco": {
-                    "rua": "Rua do Comércio",
-                    "bairro": "Centro",
-                    "cidade": "São Paulo do Potengi",
-                    "numero": "185"
-                },
-                "caracteristicas": {
-                    "tamanho": "90m²",
-                    "numQuartos": 3,
-                    "numBanheiros": 2
-                },
-                "valor": 2070000
-            },
-            {
-                "descricao": "Casa de campo com campo de futebol",
-                "proprietario": {
-                    "nome": "Joedson Ryan",
-                    "email": "Joedson.Ryan@email.com",
-                    "telefone": "5678-9012"
-                },
-                "endereco": {
-                    "rua": "Estrada da Lua",
-                    "bairro": "Rural",
-                    "cidade": "Gramado"
-                },
-                "caracteristicas": {
-                    "tamanho": "350m²",
-                    "numQuartos": 5,
-                    "numBanheiros": 4
-                },
-                "valor": 1250000
-            }
-        ]
-    }
-}
+for imovel in imoveis:
+    imoveis_dict = {}
+    imoveis_dict['descricao'] = imovel.getElementsByTagName('descricao')[0].firstChild.nodeValue
+    proprietario = imovel.getElementsByTagName('proprietario')[0]
+    imoveis_dict['nome'] = proprietario.getElementsByTagName('nome')[0].firstChild.nodeValue
+    emails =  proprietario.getElementsByTagName('email')
+    email_list = []
 
-with open('imobiliaria.json', 'w') as json_file:
-    json.dump(imobiliaria, json_file, indent=4)
+
+    for e in emails:
+        email_list.append(e .firstChild.nodeValue)
+        imoveis_dict['email'] = email_list
+    telefones =  proprietario.getElementsByTagName('telefone')
+    telefone_list = []
+
+    for t in telefones:
+        telefone_list.append(t.firstChild.nodeValue)
+        imoveis_dict['telefone'] = telefone_list
+
+
+    endereco = imovel.getElementsByTagName('endereco')[0]
+    imoveis_dict['rua'] = endereco.getElementsByTagName('rua')[0].firstChild.nodeValue
+    imoveis_dict['bairro'] = endereco.getElementsByTagName('bairro')[0].firstChild.nodeValue
+    imoveis_dict['cidade'] = cidade = endereco.getElementsByTagName('cidade')[0].firstChild.nodeValue
+
+    numeros = endereco.getElementsByTagName('numero')
+    for n in numeros: 
+        imoveis_dict['numero'] = n.firstChild.nodeValue
+
+    caracteristicas = imovel.getElementsByTagName('caracteristicas')[0]
+    imoveis_dict['tamanho'] = caracteristicas.getElementsByTagName('tamanho')[0].firstChild.nodeValue
+    imoveis_dict['numQuartos'] = caracteristicas.getElementsByTagName('numQuartos')[0].firstChild.nodeValue
+    imoveis_dict['numBanheiros'] = caracteristicas.getElementsByTagName('numBanheiros')[0].firstChild.nodeValue
+
+    list_imobiliaria.append(imoveis_dict)
+
+dict_imobiliaria['imobiliaria'] = list_imobiliaria
+with open("parses/imobiliaria.json", "w",) as json_file:
+    json.dump(dict_imobiliaria, json_file, indent=4, ensure_ascii=False)
